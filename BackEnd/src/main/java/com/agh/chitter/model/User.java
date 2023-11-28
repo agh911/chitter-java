b31e11884500c7912f706f02bef052476b160907
Andreea Ghenea <andreeag@Andreeas-MBP.cust.communityfibre.co.uk>
2023-11-28 17:13:35 +0000

    Added userService tests

--- BackEnd/src/main/java/com/agh/chitter/model/User.java
+++ BackEnd/src/main/java/com/agh/chitter/model/User.java
@@ -12,6 +12,16 @@ public class User {
     private String email;
     private String password;
 
+    public User() {
+    }
+
+    public User(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     public String getId() {
         return id;
     }
